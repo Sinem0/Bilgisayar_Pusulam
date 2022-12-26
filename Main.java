9931ecbf3cd57865b3b62d0f7ac6e42f6455a316
Sinem0 <sinemaltun0652@gmail.com>
2022-12-26 19:47:54 +0300

    Bilgisayar_Pusulam

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Kullanici kullanici = new Kullanici();
+        Sistem sistem = new Sistem(kullanici);
+        sistem.SistemGirisMenu();
+
+    }
+}
